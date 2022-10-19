93fb8e56e8487721c2e5d6f72ccb221b3cbea9a1
Amabell G. Manzanas <manzanasamabell@gmail.com>
2022-10-18 20:48:34 -0400

    update.js

--- update.js
+++ update.js
@@ -1,11 +1,11 @@
 const fs = require ('fs')
-const update = function (note, oldNote) {
-    const oNote = JSON.parse(oldNote)
 
-    const newNote = oNote.map(function(n, index){
-       if (n.id == note.id) 
-       {
-        n.title = note.title,
+const update = function (note, oldNote) {
+    
+
+    const newNote = JSON.parse(oldNote).map(function(n, i){
+       if (n.id == note.id)  {
+        n.title = note.title
         n.body =  note.body
        }
        return n
